dd1656dd663381dcf1638af8434a82ef88dc5ecc
Derek Lieu <dl@dereklieu.com>
2017-09-28 17:27:13 -0400

    Create a simple server to serve collisions

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-var assert = require('assert')
-var networkmatch = require('networkmatch')
+'use strict'
 
-module.exports = function match (data, threshold, cb) {
-  assert.deepEqual(data.type, 'FeatureCollection')
-  // default threshold of 1m
-  threshold = threshold || 0.0001
+var fs = require('fs')
+var path = require('path')
+var express = require('express')
+var generateConflation = require('./lib/generate-conflation')
 
-  data.features.forEach(function (feature) {
-    feature._id = feature.properties.name
-  });
-  networkmatch.index(data)
-  var results = data.features.map(matchFeature)
-  cb(null, results)
+var PORT = 3010
 
-  function matchFeature (feature) {
-    return networkmatch.match(feature, threshold)
-  }
+function createServer (collisions) {
+  var server = express()
+  server.get('/collisions', (req, res) => {
+    res.json(collisions.map(c => c._id))
+  })
+  server.get('/collision/:id', (req, res) => {
+    let record = collisions.find(c => c._id === req.params.id)
+    if (!record) res.status(404).end()
+    else res.json(record)
+  })
+  server.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log('Server running on port', PORT)
+  })
 }
+
+// eslint-disable-next-line no-undef
+fs.readFile(path.join(__dirname, 'data/sample.geojson'), (err, file) => {
+  if (err) { throw new Error(err) }
+  var network = JSON.parse(file)
+  generateConflation(network, (err, collisions) => {
+    if (err) { throw new Error(err) }
+    createServer(collisions)
+  })
+})
